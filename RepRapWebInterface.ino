52dcce905506e4b74c2510b89a852671c2c1a4ef
reprappro <reprappro@gmail.com>
2012-12-13 21:43:42 +0000

    Atempting to control cacheing - not entirely successfully...

--- RepRapWebInterface.ino
+++ RepRapWebInterface.ino
@@ -34,16 +34,18 @@ char line[1000];
 char page[1000];
 int lp;
 
-void setup()
+void setup() // Thanks to SurferTim: http://arduino.cc/forum/index.php?action=profile;u=49379
 {
   lp = 0;
   for(int i=0; i < MAX_FILES; i++)
     inUse[i] = false;
-    
+
   Serial.begin(9600);
 
-  pinMode(10, OUTPUT);
-  digitalWrite(10,LOW);   
+  // disable SD SPI while starting w5100
+  // or you will have trouble
+  pinMode(4, OUTPUT);
+  digitalWrite(4,HIGH);   
 
   Ethernet.begin(mac, ip);
   server.begin();
@@ -51,17 +53,16 @@ void setup()
   Serial.print("server is at ");
   Serial.println(Ethernet.localIP());
   
+  // this corrects a bug in the Ethernet.begin() function
+  // even tho the call to Ethernet.localIP() does the same thing
   digitalWrite(10,HIGH);
  
   if (!SD.begin(4)) 
      Serial.println("SD initialization failed.");
-    
-  digitalWrite(10,LOW); 
-     
-  Serial.print("server is (still?) at ");
-  Serial.println(Ethernet.localIP());    
-     
+  // SD.begin() returns with the SPI disabled, so you need not disable it here  
 }
+
+
 
 void error(char* s)
 {
@@ -125,10 +126,10 @@ bool Read(int file, unsigned char* b)
 }
 
 
-void parseLine()
+bool parseLine()
 {
   if(!(line[0] == 'G' && line[1] == 'E' && line[2] == 'T'))
-    return;
+    return false;
   int i = 5;
   int j = 0;
   while(line[i] != ' ')
@@ -140,12 +141,15 @@ void parseLine()
   page[j] = 0;
   if(!page[0])
     strcpy(page, "index.htm");
+  j = strlen(page);
+  return(page[j-3] == 'g' && page[j-2] == 'i' && page[j-1] == 'f');
 }
 
 
 void loop() 
 {
   int htmlFile;
+  bool loadingImage;
   // listen for incoming clients
   EthernetClient client = server.available();
   if (client) 
@@ -153,6 +157,7 @@ void loop()
     comment("new client");
     // an http request ends with a blank line
     boolean currentLineIsBlank = true;
+    loadingImage = false;
     while (client.connected()) 
     {
       if (client.available()) 
@@ -167,6 +172,11 @@ void loop()
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
           client.println("Connnection: close");
+ /*         if(loadingImage)
+          {
+            client.println("Cache-Control: max-age=3600");
+            Serial.println("Image requested");
+          }*/
           client.println();
       
           htmlFile = OpenFile(page,false);
@@ -182,7 +192,8 @@ void loop()
         if (c == '\n') 
         {
           line[lp] = 0;
-          parseLine();
+          if(parseLine())
+            loadingImage = true;
           // you're starting a new line
           currentLineIsBlank = true;
           lp = 0;
